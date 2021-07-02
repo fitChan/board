1f911f20b3f5e84b6cb2c42417549b8c40620444
fitChan <84306157+fitChan@users.noreply.github.com>
2021-07-02 11:08:14 +0900

    Initial commit

---
+++ src/main/java/com/sparta/board/domain/entity/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.board.domain.entity;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findAllByOrderByModifiedDateDesc();
+}
